6a3fabe0f276838f14709a469a8b2e790544284b
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-07-01 00:58:08 +0100

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
 # COmment
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install werkzeug==2.1.1',
 }
