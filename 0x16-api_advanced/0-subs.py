8f3e3b7deae5b9ed7f1f747bd179a098493888b5
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-08-14 01:18:33 +0100

    again

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return number of subscribers in subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """Return the number of subscribers for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "Agent"
+        "User-Agent": "linux:0-subs:v1.0 (by /u/your_reddit_username)"
     }
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
